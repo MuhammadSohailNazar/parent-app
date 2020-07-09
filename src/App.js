2885a3701dc93daebe8693d45c4a094f1f22fa95
Msn <sohail.nazar@outlook.com>
2020-07-09 13:21:00 +0500

    cahgnes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { TodaysDate } from 'react-lib';
+
+
+function App() {
+  return (
+    <div className="App">
+     <TodaysDate />
+    </div>
+  );
+}
+
+export default App;
